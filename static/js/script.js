8d7dda41dd55fde40b4595414b10375488d93c44
Elroyyy <elroypushparajah@gmail.com>
2026-02-23 16:14:28 +0530

    Image slider Update

--- static/js/script.js
+++ static/js/script.js
@@ -733,3 +733,52 @@ document.addEventListener('DOMContentLoaded', function() {
     // Uncomment when API is ready
     // loadSermons();
 });
+
+let slideIndex = 1;
+let slideTimer;
+showSlides(slideIndex);
+startSlider();
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+function startSlider() {
+  slideTimer = setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+  }, 2500);
+}
+
+// Stop slider
+function stopSlider() {
+  clearInterval(slideTimer);
+}
+
+// Pause on hover
+const slider = document.querySelector(".slideshow-container");
+
+slider.addEventListener("mouseenter", stopSlider);
+slider.addEventListener("mouseleave", startSlider);
